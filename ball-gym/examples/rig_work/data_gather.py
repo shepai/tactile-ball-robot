d76e3a63e53a81bfe3a47935afdad1f2bc63227a
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2026-07-08 15:15:34 +0100

    updates

--- ball-gym/examples/rig_work/data_gather.py
+++ ball-gym/examples/rig_work/data_gather.py
@@ -44,7 +44,10 @@ total_samples = (
     6 *
     len(np.arange(0,0.2,0.1)) *
     len(np.arange(0,0.2,0.1)) *
-    len(np.arange(0,0.05,0.001))
+    len(np.arange(0,0.05,0.001)) *
+    len(np.arange(-np.pi/2,np.pi/2,(np.pi)/5)) * 
+    len(np.arange(-np.pi/2,np.pi/2,(np.pi)/5)) * 
+    len(np.arange(-np.pi/2,np.pi/2,(np.pi)/5)) 
 )
 
 with tqdm(total=total_samples, desc="Collecting data") as pbar:
@@ -56,87 +59,99 @@ with tqdm(total=total_samples, desc="Collecting data") as pbar:
             env.unwrapped.set_texture(texture_id)
             for i in np.arange(0,0.2,0.1):
                 for j in np.arange(0,0.2,0.1):
-                    for k in np.arange(0,0.05,0.001):
-
-                        target = start.copy()
-                        target[0] = start[0] + i
-                        target[1] = start[1] + j
-                        target[2] = start[2] - k
-
-                        obs, reward, terminated, truncated, info = env.step(target.copy())
-
-
-                        # Save tactile image
-                        image_name = f"{c}.png"
-                        image_path = os.path.join(
-                            DATA_SAVE_PATH,
-                            "images",
-                            image_name
-                        )
-
-                        left_view = cv2.cvtColor(
-                            obs['sensor_cam_left']*255,
-                            cv2.COLOR_RGB2BGR
-                        ).astype(np.uint8)
-                        gray = cv2.cvtColor(
-                            left_view,
-                            cv2.COLOR_BGR2GRAY
-                        )
-                        cv2.imwrite(image_path, gray,[cv2.IMWRITE_PNG_COMPRESSION, 9])
+                    for thetx in np.arange(-np.pi/2,np.pi/2,(np.pi)/5):
+                        for thety in np.arange(-np.pi/2,np.pi/2,(np.pi)/5):
+                            for thetz in np.arange(-np.pi/2,np.pi/2,(np.pi)/5):
+                                for k in np.arange(0,0.05,0.001):
+                                    target = start.copy()
+                                    target[0] = start[0] + i
+                                    target[1] = start[1] + j
+                                    target[2] = start[2] - k
+                                    target[3]=thetx
+                                    target[4]=thety
+                                    target[5]=thetz
+                                    obs, reward, terminated, truncated, info = env.step(target.copy())
 
 
-                        # Get marker positions
-                        markers = env.unwrapped.get_markers()
+                                    # Save tactile image
+                                    image_name = f"{c}.png"
+                                    image_path = os.path.join(
+                                        DATA_SAVE_PATH,
+                                        "images",
+                                        image_name
+                                    )
 
-                        row = {
-                            "image_id": c,
-                            "x": target[0],
-                            "y": target[1],
-                            "z": target[2],
-                            "speed": speed,
-                            "texture_id": texture_id,
-                        }
-
-
-                        # Add markers dynamically
-                        for marker_id, marker in enumerate(markers):
-                            row[f"marker_{marker_id}_x"] = marker[0]
-                            row[f"marker_{marker_id}_y"] = marker[1]
-                            row[f"marker_{marker_id}_z"] = marker[2]
-
-
-                        recordings.append(row)
-
-
-                        c += 1
-                        if c % 100 == 0:
-                            df = pd.DataFrame(recordings)
-
-                            csv_path = os.path.join(
-                                DATA_SAVE_PATH,
-                                "recordings.csv"
-                            )
-
-                            # Append if file already exists
-                            df.to_csv(
-                                csv_path,
-                                mode="a",
-                                header=not os.path.exists(csv_path),
-                                index=False
-                            )
+                                    left_view = cv2.cvtColor(
+                                        obs['sensor_cam_left']*255,
+                                        cv2.COLOR_RGB2BGR
+                                    ).astype(np.uint8)
+                                    gray = cv2.cvtColor(
+                                        left_view,
+                                        cv2.COLOR_BGR2GRAY
+                                    )
+                                    image = cv2.resize(
+                                        gray,
+                                        (64, 48),
+                                        interpolation=cv2.INTER_AREA
+                                    )
+                                    cv2.imwrite(image_path, image,[cv2.IMWRITE_PNG_COMPRESSION, 9])
 
 
-                        folder_size = get_folder_size(DATA_SAVE_PATH)
-                        free_space = get_disk_space(DATA_SAVE_PATH)
+                                    # Get marker positions
+                                    markers = env.unwrapped.get_markers()
 
-                        pbar.set_postfix({
-                            "data": f"{folder_size:.2f} GB",
-                            "free": f"{free_space:.2f} GB"
-                        })
-                        if folder_size >= free_space-50-folder_size: #keep 50GB
-                            print("Storage limit reached. Stopping collection.")
-                            exit()
-                        pbar.update(1)
+                                    row = {
+                                        "image_id": c,
+                                        "x": target[0],
+                                        "y": target[1],
+                                        "z": target[2],
+                                        "theta_x":target[3],
+                                        "theta_y":target[4],
+                                        "theta_z":target[5],
+                                        "speed": speed,
+                                        "texture_id": texture_id,
+                                    }
+
+
+                                    # Add markers dynamically
+                                    for marker_id, marker in enumerate(markers):
+                                        row[f"marker_{marker_id}_x"] = marker[0]
+                                        row[f"marker_{marker_id}_y"] = marker[1]
+                                        row[f"marker_{marker_id}_z"] = marker[2]
+
+
+                                    recordings.append(row)
+
+
+                                    c += 1
+                                    if c % 100 == 0:
+                                        df = pd.DataFrame(recordings)
+
+                                        csv_path = os.path.join(
+                                            DATA_SAVE_PATH,
+                                            "recordings.csv"
+                                        )
+
+                                        # Append if file already exists
+                                        df.to_csv(
+                                            csv_path,
+                                            mode="a",
+                                            header=not os.path.exists(csv_path),
+                                            index=False
+                                        )
+
+
+                                    folder_size = get_folder_size(DATA_SAVE_PATH)
+                                    free_space = get_disk_space(DATA_SAVE_PATH)
+
+                                    pbar.set_postfix({
+                                        "data": f"{folder_size:.2f} GB",
+                                        "free": f"{free_space:.2f} GB"
+                                    })
+                                    if folder_size >= free_space-50-folder_size: #keep 50GB
+                                        print("Storage limit reached. Stopping collection.")
+                                        exit()
+                                    pbar.update(1)
 
 # Convert to dataframe
 df = pd.DataFrame(recordings)
